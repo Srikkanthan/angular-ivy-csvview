0b12cc6b6e6515cf454b2fce52b8cd829e2fb24c
Srikkanthan <srikkanthan@outlook.com>
2022-10-30 11:01:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-csvview
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pcs8au)
---
+++ src/components/csv-view/csv-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'csv-view',
+  templateUrl: './csv-view.component.html',
+  styleUrls: ['./csv-view.component.css'],
+})
+export class CsvView {}
